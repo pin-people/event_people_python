8b97751968efd0a607f27579b0c96918f2e75757
Bruno <bmallia10@gmail.com>
2022-10-24 17:51:22 -0300

    sc-6157 fixing to send event's header in the emiter

--- event_people/event.py
+++ event_people/event.py
@@ -8,16 +8,17 @@ import ast
 
 class Header:
 
-    def __init__(self, app, resource, origin, action, destination):
+    def __init__(self, app, resource, origin, action, destination, schema_version):
         self.app = app
         self.resource = resource
         self.origin = origin
         self.action = action
         self.destination = destination
-        self.schema_version =  1.0
+        self.schema_version = schema_version
 
     def __str__(self):
         return f'{self.app}.{self.resource}.{self.origin}.{self.action}.{self.destination}.{self.schema_version}'
+
 
 class Event(object):
     """
@@ -27,19 +28,19 @@ class Event(object):
 
     def __init__(self, name, body, schema_version = 1.0):
         self.name = self.__fix_name__(name)
-        self.__generate_header__()
+        self.__generate_header__(schema_version)
         body_dict = body
 
         if not isinstance(body, dict):
             body_dict = ast.literal_eval(body.decode('utf-8'))
 
-        self.header.schema_version = schema_version
+        self.header['schema_version'] = schema_version
 
         self.body = body_dict if 'body' not in body_dict else body_dict['body']
 
-    def __generate_header__(self):
+    def __generate_header__(self, schema_version):
         resource, origin, action, destination = self.name.split(".")
-        self.header = Header(app=self.APP_NAME ,resource=resource, origin=origin, action=action, destination=destination)
+        self.header = Header(app=self.APP_NAME ,resource=resource, origin=origin, action=action, destination=destination, schema_version=schema_version).__dict__
 
     def __fix_name__(self, name):
         if len(name.split('.')) < 3:
