23a79ab8031a676c27a9026fac6db6b8324a6ea3
Bruno <bmallia10@gmail.com>
2022-10-25 09:53:23 -0300

    sc-5157 - fix code review

--- event_people/broker/rabbit/topic.py
+++ event_people/broker/rabbit/topic.py
@@ -28,6 +28,6 @@ class Topic:
 
     def iproduce(self, event):
         topic = self.iget_topic()
-        body = json.dumps(event.__dict__, indent=2).encode('utf-8')
+        body = json.dumps(event.payload(), indent=2).encode('utf-8')
 
         topic.basic_publish(exchange=self.TOPIC_NAME, routing_key=event.name, body=body)
